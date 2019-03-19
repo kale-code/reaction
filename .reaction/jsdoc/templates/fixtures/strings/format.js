dfefc03894d66c2d10c9d4c50c9a30622f0fe013
kale-io <cos+a@iuvo.ai>
2019-03-19 18:08:20 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- .reaction/jsdoc/templates/fixtures/strings/format.js
+++ .reaction/jsdoc/templates/fixtures/strings/format.js
@@ -23,10 +23,7 @@
  */
 module.exports = function ( format ) {
 	var args = Array.prototype.slice.call( arguments, 1 );
-	return format.replace( /{(\d+)}/g, function ( match, number ) {
-		return typeof args[number] != 'undefined'
+	return format.replace( /{(\d+)}/g, ( match, number ) => typeof args[number] != 'undefined'
 			? args[number]
-			: match
-			;
-	} );
+			: match );
 };
